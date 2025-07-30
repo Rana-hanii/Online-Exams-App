6f0bd348af25a225505bf1e39f227c5638cd5f3c
Rana-hanii <ranahani797@gmail.com>
2025-07-31 01:23:04 +0300

    Add Auth lib and Update on interfaces and adaptors

--- projects/auth-api/src/lib/adaptor/auth-api.adaptor.ts
+++ projects/auth-api/src/lib/adaptor/auth-api.adaptor.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Adaptor } from '../interfaces/adaptor';
+import { IForgetPasswordRes } from '../interfaces/forget-password/IForgetPasswordRes';
+import { ILogOutRes } from '../interfaces/log-out/ILogOutRes';
+import { IResetPasswordRes } from '../interfaces/reset-password/IResetPasswordRes';
+import { ISignInRes } from '../interfaces/sign-in/ISignInRes';
+import { ISignUpRes } from '../interfaces/sign-up/ISignUpRes';
+import { ICodeRes } from '../interfaces/verify-code/ICodeRes';
 
 @Injectable({
   providedIn: 'root',
@@ -8,42 +14,50 @@ export class AuthAPIAdaptorService implements Adaptor {
   constructor() {}
 
   // * ADAPTOR METHODS FOR SIGNUP RESPONSE
-  adaptSignUp(data: any) {
-    return {};
+  adaptSignUp(data: ISignUpRes) {
+    return {
+      message: data.message,
+      username:data.user.username,
+      email:data.user.email
+    };
   }
 
   // * ADAPTOR METHODS FOR SIGN IN RESPONSE
-  adaptSignIn(data: any) {
+  adaptSignIn(data: ISignInRes ) {
     return {
       message: data.message,
       token: data.token,
-      email: data.user.email,
+      username:data.user.username
     };
   }
 
   // * ADAPTOR METHODS FOR LOGOUT RESPONSE
-   adaptLogOut(data: any) {
-    return {};
+  adaptLogOut(data: ILogOutRes) {
+    return {
+      message: data.message,
+    };
   }
 
   // * ADAPTOR METHODS FOR FORGET PASS RESPONSE
 
-  adaptForgetPassword(data: any) {
+  adaptForgetPassword(data:IForgetPasswordRes) {
     return {
       message: data.message,
+      info: data.info,
     };
   }
   // * ADAPTOR METHODS FOR VERIFY CODE RESPONSE
-  adaptVerifyCode(data: any) {
+  adaptVerifyCode(data: ICodeRes) {
     return {
-      message: data.message,
+      status: data.status,
     };
   }
 
   // * ADAPTOR METHODS FOR RESET PASS RESPONSE
-  adaptResetPassword(data: any) {
+  adaptResetPassword(data:IResetPasswordRes) {
     return {
       message: data.message,
+      token:data.token,
     };
   }
 }
--- projects/auth-api/src/lib/base/authAPI.ts
+++ projects/auth-api/src/lib/base/authAPI.ts
@@ -1,10 +1,21 @@
 import { Observable } from 'rxjs';
+import { ISignUpReq } from '../interfaces/sign-up/ISignUpReq';
+import { ISignUpRes } from '../interfaces/sign-up/ISignUpRes';
+import { ISignInReq } from '../interfaces/sign-in/ISignInReq';
+import { ISignInRes } from '../interfaces/sign-in/ISignInRes';
+import { ILogOutRes } from '../interfaces/log-out/ILogOutRes';
+import { IForgetPasswordReq } from '../interfaces/forget-password/IForgetPasswordReq';
+import { IForgetPasswordRes } from '../interfaces/forget-password/IForgetPasswordRes';
+import { ICodeReq } from '../interfaces/verify-code/ICodeReq';
+import { ICodeRes } from '../interfaces/verify-code/ICodeRes';
+import { IResetPasswordReq } from '../interfaces/reset-password/IResetPasswordReq';
+import { IResetPasswordRes } from '../interfaces/reset-password/IResetPasswordRes';
 
 export abstract class authAPI {
-  abstract SignUp(data: any): Observable<any>;
-  abstract SignIn(data: any): Observable<any>;
-  abstract LogOut(data: any): Observable<any>;
-  abstract ForgetPassword(data: any): Observable<any>;
-  abstract VerifyCode(data: any): Observable<any>;
-  abstract ResetPassword(data: any): Observable<any>;
+  abstract SignUp(data: ISignUpReq): Observable<ISignUpRes>;
+  abstract SignIn(data: ISignInReq): Observable<ISignInRes>;
+  abstract LogOut(): Observable<ILogOutRes>;
+  abstract ForgetPassword(data: IForgetPasswordReq): Observable<IForgetPasswordRes>;
+  abstract VerifyCode(data: ICodeReq): Observable<ICodeRes>;
+  abstract ResetPassword(data: IResetPasswordReq): Observable<IResetPasswordRes>;
 }
