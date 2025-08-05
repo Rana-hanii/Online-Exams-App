32c8e04ed835118df5a9a62adec9ddbbb9972d82
Rana-hanii <ranahani797@gmail.com>
2025-08-05 19:16:20 +0300

    Add Sign in UI

--- src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthApiService } from 'auth-api';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
+import { DividerAndIconsComponent } from "../../components/divider-and-icons/divider-and-icons.component";
+
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    RouterLink,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    MessageModule,
+    PasswordModule,
+    DividerAndIconsComponent
+],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  messageService = inject(MessageService);
+  _AuthApiService= inject(AuthApiService);
+  private router = inject(Router);
+  signInForm: FormGroup;
+  formSubmitted = false;
+  isSubmitting = false;
 
+
+
+  constructor(private fb: FormBuilder) {
+    this.signInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)]],
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.signInForm.invalid) {
+      return;
+    }
+    this.isSubmitting = true;
+    this._AuthApiService.SignIn(this.signInForm.value).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token);
+        console.log(res);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Sign in successful' });
+        this.router.navigate(['']);
+      },
+      error: (err) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: err.error.message });
+      }
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.signInForm.get(controlName);
+    return control?.invalid && (control.touched || this.formSubmitted);
+  }
 }
