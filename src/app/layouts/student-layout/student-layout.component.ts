e2231c6210ad6dce160f89610cd08881b168a1e6
Rana-hanii <ranahani797@gmail.com>
2025-08-23 19:56:08 +0300

    Add Student layout UI And Student Dashboard

--- src/app/layouts/student-layout/student-layout.component.ts
+++ src/app/layouts/student-layout/student-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "../../features/student/components/side-bar/side-bar.component";
+import { HeaderComponent } from "../../features/student/components/header/header.component";
 
 @Component({
   selector: 'app-student-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent, HeaderComponent],
   templateUrl: './student-layout.component.html',
   styleUrl: './student-layout.component.css'
 })
