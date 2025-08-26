77b816272f466622a1c57794b8a273673ad558f8
Rana-hanii <ranahani797@gmail.com>
2025-08-26 03:30:25 +0300

    Add  withInMemoryScrolling on routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,38 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter, withHashLocation } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import Aura from '@primeng/themes/aura';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers:
-   [provideHttpClient(withFetch()),
+  providers: [
+    provideHttpClient(withFetch()),
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes, withHashLocation()), provideClientHydration(withEventReplay()),
-     provideAnimationsAsync(),
-     MessageService,
-           providePrimeNG({
-          theme: {
-              preset: Aura
-          }
-      })]
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions(),
+      withHashLocation()
+    ),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    MessageService,
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
