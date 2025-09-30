3ba9d798a7abc49cff4481b6600ee47d1c4afa58
Rana-hanii <ranahani797@gmail.com>
2025-09-30 20:21:27 +0300

    Fix:(Fix build error)

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'student/exam-modal/:examId',
+    renderMode: RenderMode.Server   
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
+
