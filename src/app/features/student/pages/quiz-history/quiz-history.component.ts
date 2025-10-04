846727642d8621bfcf9c7f0ce67f26623f059ad6
Rana-hanii <ranahani797@gmail.com>
2025-10-04 22:03:01 +0300

    Revert "Feat:(save exams history to localstorage )"
    This reverts commit 0e451b788df9d6c4ebd119e95d351d4f3fc99dc8.

--- src/app/features/student/pages/quiz-history/quiz-history.component.ts
+++ src/app/features/student/pages/quiz-history/quiz-history.component.ts
@@ -1,28 +1,11 @@
-import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { AppState } from '../../../../store/app.state';
-import { openQuestionModal } from '../../../../store/questions/questions.actions';
-import { ExamHistory } from '../../../../shared/interfaces/exam.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-history',
   imports: [],
   templateUrl: './quiz-history.component.html',
-  styleUrl: './quiz-history.component.css',
+  styleUrl: './quiz-history.component.css'
 })
 export class QuizHistoryComponent {
-  historyExams: ExamHistory[] = [];
-  router = inject(Router);
 
-  // constructor(private store: Store<AppState>) {}
-
-  ngOnInit(): void {
-    this.historyExams = JSON.parse(localStorage.getItem('examHistory') || '[]');
-  }
-
-  showResult(examId: string): void {
-    console.log('Show result for exam:', examId);
-    this.router.navigate(['/student/exam-modal', examId]);
-  }
 }
